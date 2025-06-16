8beb5f5247768a1383000ec4b47f2bc435a96cea
Andrew 'ImDoctor <130654327+xImDoctor@users.noreply.github.com>
2025-06-17 00:05:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.set_page_config(
 st.sidebar.title("Настройки модели")
 model_name = st.sidebar.selectbox(
     "Выберите модель", 
-    ("resnet-word-trained-ver3", "resnet-word-trained-ver2", "model-finetuned")
+    ("resnet-word-trained-ver3", "resnet-word-trained-ver2", "model_finetuned")
 )
 
 @st.cache_resource
